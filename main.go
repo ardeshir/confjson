b2b0c963a9b07d4ffce112159e74294582109897
ardeshir sepahsalar <ardeshir.org@gmail.com>
2016-08-26 14:45:38 +0000

    fixed typo

--- main.go
+++ main.go
@@ -7,26 +7,24 @@ import (
 )
 
 type config struct {
-	Enable bool
-	Path   string
-	Name   string
+	Enabled bool
+	Path    string
+	Name    string
 }
 
 func main() {
 
-   file, _ := os.Open("conf.json")
-   defer file.Close()
+	file, _ := os.Open("conf.json")
+	defer file.Close()
 
-   decode := json.NewDecoder(file)
-   conf := config{}
-   err := decoder.Decode(&conf)
-   if err != nil {
-	fmt.Println("Error:", err)
-   }
+	decoder := json.NewDecoder(file)
+	conf := config{}
+	err := decoder.Decode(&conf)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 
-   fmt.Println(conf.Path)
-   fmt.Println(conf.Name)
-   fmt.Println(conf.Enabled)
+	fmt.Println(conf.Path)
+	fmt.Println(conf.Name)
+	fmt.Println(conf.Enabled)
 }
-
-	
